9af9e6a0e3e105863c377a07d3680b0c55156707
jseepaul1 <seepauljulie@gmail.com>
2022-04-12 23:33:21 -0400

    minor updates to countries query

--- countries_query.sql
+++ countries_query.sql
@@ -3,34 +3,35 @@ SELECT * FROM countries;
 SELECT * FROM languages;
 -- What query would you run to get all the countries that speak Slovene? Your query should return the name of the country, language and language percentage. 
 -- Your query should arrange the result by language percentage in descending order. 
-SELECT * FROM countries
+SELECT countries.name, languages.language, languages.percentage 
+FROM countries
 JOIN languages ON countries.id = languages.country_id
 WHERE language = 'Slovene'
 ORDER BY percentage DESC;
 
 -- What query would you run to display the total number of cities for each country? Your query should return the name of the country and the total number of cities.
 --  Your query should arrange the result by the number of cities in descending order
-SELECT countries.name, COUNT(countries.id) FROM countries 
+SELECT countries.name, COUNT(cities.id) FROM countries 
 JOIN cities ON countries.id = cities.country_id 
 GROUP BY countries.id
-ORDER BY COUNT(countries.id) DESC;
+ORDER BY COUNT(cities.id) DESC;
 
 --  What query would you run to get all the cities in Mexico with a population of greater than 500,000? Your query should arrange the result by population in descending order.
 SELECT cities.name, cities.population
 FROM cities
 JOIN countries ON cities.country_id = countries.id
-WHERE countries.name = 'Mexico' AND cities.population >500000
+WHERE countries.name = 'Mexico' AND cities.population > 500000
 ORDER BY cities.population DESC;
 
 -- What query would you run to get all languages in each country with a percentage greater than 89%? Your query should arrange the result by percentage in descending order. 
-SELECT countries.name, languages.percentage
+SELECT countries.name, languages.percentage, languages.language
 FROM countries
 JOIN languages ON countries.id = languages.country_id
-WHERE languages.percentage >89
+WHERE languages.percentage > 89
 ORDER BY languages.percentage DESC;
 
 -- What query would you run to get all the countries with Surface Area below 501 and Population greater than 100,000?
-SELECT countries.name,countries.surface_area, countries.population
+SELECT countries.name,countries.surface_area,countries.population
 FROM countries
 WHERE countries.surface_area < 501 AND countries.population > 100000;
 
@@ -49,7 +50,7 @@ ORDER BY cities.population DESC;
 
 -- What query would you run to summarize the number of countries in each region? The query should display the name of the region and the number of countries.
 -- Also, the query should arrange the result by the number of countries in descending order
-SELECT countries.region, COUNT(countries.name)
+SELECT countries.region, COUNT(countries.id)
 FROM countries
 GROUP BY countries.region
-ORDER BY COUNT(countries.name) DESC
+ORDER BY COUNT(countries.id) DESC;
